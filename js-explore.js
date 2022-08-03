3615966da7f609cbace3efe94f030837be45c97b
nwebpro <nwebpro.work@gmail.com>
2022-08-03 17:50:15 +0600

    All Problem solved

---
+++ js-explore.js
@@ -0,0 +1,8 @@
+var bookPrice = [120, 200, 300, 420, 150, 180, 79];
+ for(var i = 0; i < bookPrice.length; i++){
+    var priceList = bookPrice[i];
+    if(priceList >= 200){
+        continue;
+    }
+    console.log(priceList)
+ }
